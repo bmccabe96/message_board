398f1638b9b52d1184ed0521dad33f619e9fcc0e
bmccabe96 <brianmccabe96@gmail.com>
2022-05-12 22:07:08 -0400

    Added post handling for form submission that adds message to array

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 
-const defaultMessages = [
+const messages = [
   {
     text: "This message board is poppin",
     user: "Charles",
@@ -19,11 +19,23 @@ const defaultMessages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: defaultMessages });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 router.get('/new', function(req, res, next) {
   res.render('form', { title: 'Mini Messageboard'} );
 });
+
+router.post('/new', function(req, res, next) {
+  const newMessage = {
+    text: req.body.text, 
+    user: req.body.user, 
+    added: moment()
+      .startOf('hour'- 1)
+      .fromNow(),
+  };
+  messages.unshift(newMessage);
+  res.redirect('/');
+})
 
 module.exports = router;
